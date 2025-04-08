2adfe1640d1233ce6f86ca838d36cce3423e60a1
Romaso1 <90009050+Romaso1@users.noreply.github.com>
2025-04-08 21:00:27 +0300

    Add files via upload

---
+++ Lab2.java
@@ -0,0 +1,14 @@
+public class Lab2 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println("Елементи масиву:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        System.out.println("Сума елементів масиву: " + sum);
+    }
+}
